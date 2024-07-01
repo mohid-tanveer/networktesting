e088766fd663ac01cd0e97afcd78f37ae9a632fb
mohid tanveer <mohid.tanveer011@gmail.com>
2024-07-01 16:26:21 -0500

    change to multithreading test

--- scripts/multiautomator.py
+++ scripts/multiautomator.py
@@ -1,7 +1,6 @@
 import os
-import time
 import socket
-from secret import cf, sp2, turn_order
+from secret import sp2, sp3
 
 round_complete = False
 
@@ -11,40 +10,16 @@ host_name = socket.gethostname()
 # set up protocol node choices
 pn_choices = ['pn001', 'pn002', 'pn003', 'pn004', 'pn005', 'pn006']
 
-# control file path (./control.txt)
-CONTROL_FILE = cf
-
 # local script path (./filetransfer.py)
 SCRIPT_PATH = sp2
-
-# check if it's this machine's turn to run the script
-def check_turn():
-    with open(CONTROL_FILE, "r") as file:
-        current_control = file.read().strip()
-    return current_control == host_name
-
-# Function to update control file to switch to the other machine
-def switch_turn():
-    next_machine = turn_order[(turn_order.index(host_name) + 1) % len(turn_order)]
-    with open(CONTROL_FILE, "w") as f:
-        f.write(next_machine)
+SCRIPT_PATH2 = sp3
 
 # check and run loop
 while True:
     # alternate between protocol nodes
     for pn in pn_choices:
-        # reset round complete
-        round_complete = False
-        while not round_complete:
-            # check if local machine's turn
-            if check_turn():
-                print(f"Running script")
-                arg = pn
-                os.system(f"python {SCRIPT_PATH} {arg}")
-                switch_turn()
-                # indicate that round is complete
-                round_complete = True
-            else:
-                # if not wait before checking again
-                print(f"Waiting for turn on protocol node {pn}...")
-                time.sleep(10)
+        print(f"Running single threaded script")
+        arg = pn
+        os.system(f"python {SCRIPT_PATH2} {arg}")
+        print(f"Running multi threaded script")
+        os.system(f"python {SCRIPT_PATH} {arg}")
--- scripts/singlethreaded.py
+++ scripts/singlethreaded.py
@@ -14,44 +14,45 @@ curr_pn = sys.argv[1]
 # remote path to the folder with the files to be read
 remote_path = pnpaths[int(curr_pn[-1]) - 1]
 
-def read_single_file(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-    return 
-
-def read_files_from_directory_multi(directory):
+def read_files_from_directory_single(directory):
     elapsed_time = 0
-    file_total_size = 10737418240
-    tasks = []
-    start_time = time.time()
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        for root, dirs, files in os.walk(directory):
-            # iterate over all files in the directory
-            # in the case of one file, will only be one iteration
-            for file in files:
-                # get individual file path and submit a task to read the file
-                file_path = os.path.join(root, file)
-                tasks.append(executor.submit(read_single_file, file_path))
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    file_total_size = 0
+    
+    for root, dirs, files in os.walk(directory):
+        # iterate over all files in the directory
+        # in the case of one file, will only be one iteration
+        for file in files:
+            # get individual file path and read the file
+            file_path = os.path.join(root, file)
+            # get file size and add to total file size
+            file_size = os.path.getsize(file_path)
+            file_total_size += file_size
+            temp_start_time = time.time()
+            with open(file_path, 'r') as file:
+                content = file.read()
+            temp_end_time = time.time()
+            temp_elapsed_time = temp_end_time - temp_start_time
+            # add the elapsed time to the total elapsed time
+            elapsed_time += temp_elapsed_time
+    
     # calculate the transfer speed
     transfer_speed = file_total_size / elapsed_time
 
     return elapsed_time, file_total_size, transfer_speed
 
-def networktesting_multi(directory_path):
+def networktesting_single(directory_path):
     # init the list to store the read times tuples
-    multi_read_times = []
+    single_read_times = []
     # get the folder name
     folder_name =  os.path.basename(directory_path)
     # read files from directory and get the time taken, file size and transfer speed
-    # multi-threaded
-    time_taken, file_size, transfer_speed = read_files_from_directory_multi(directory_path)
-    print(f"Total time taken for multi-threaded {folder_name} test: {time_taken} seconds")
+    # single-threaded
+    time_taken, file_size, transfer_speed = read_files_from_directory_single(directory_path)
+    print(f"Total time taken for single-threaded {folder_name} test: {time_taken} seconds")
     t = time.localtime()
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
     # append the tuple to the read times list
-    multi_read_times.append((time_taken, current_time, file_size, transfer_speed, curr_pn, 'multi'))
+    single_read_times.append((time_taken, current_time, file_size, transfer_speed, curr_pn, 'single'))
     # check if a results file exists
     exists = True if os.path.exists(rf'{remote_path}\results\{host_name}_{folder_name}.csv') else False
     # if result file doesn't exist, create it and write the headers
@@ -64,14 +65,13 @@ def networktesting_multi(directory_path):
     # write the results to a csv file
     with open(rf'{remote_path}\results\{host_name}_{folder_name}_multi.csv', mode='a') as file:
         writer = csv.writer(file)
-        for row in multi_read_times:
+        for row in single_read_times:
             writer.writerow(row)
 
 def main():
     print(f"Testing {curr_pn}")
     # test the network transfer speed of the specified directory/directories
-    print("testing m-t tenmegfiles")
-    networktesting_multi(rf'{remote_path}\tenmegfiles')
+    print("testing s-t tenmegfiles")
+    networktesting_single(rf'{remote_path}\tenmegfiles')
     
-
 main()
